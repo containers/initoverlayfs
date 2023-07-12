1047cfcddd235d8d21544f92eff2b0a5a80a411a
Eric Curtin <ecurtin@redhat.com>
2023-07-12 13:26:44 +0100

    some install steps

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 
 set -e
 
+release=$(uname -r)
+
+skipcpio /boot/initramfs-$release.img | zcat | cpio -ivd
 gcc -O3 -pedantic -Wall -Wextra initoverlayfs2init.c -o /boot/initoverlayfs/usr/bin/initoverlayfs2init
+sed -i '/^initrd /d' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
+sed -i 's#options root=UUID=2f8957f6-9ecd-480f-b738-41d6da946bf4 ro rhgb quiet#root=/dev/vda3 ro rhgb quiet rootfstype=ext4 rootwait init=/usr/bin/initoverlayfs2init#g' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
+sed -i "s/UUID=2aadcf0d-81dc-4b21-99ef-74b96bb357ad/# UUID=2aadcf0d-81dc-4b21-99ef-74b96bb357ad/g" /etc/fstab
+systemctl daemon-reload
+dracut -f
 
