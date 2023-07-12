a5614e611d41ffec297357ede87300b1f178a381
Eric Curtin <ecurtin@redhat.com>
2023-07-12 13:34:14 +0100

    function

--- build.sh
+++ build.sh
@@ -4,14 +4,18 @@ set -ex
 
 release=$(uname -r)
 
-cd /boot/initoverlayfs/
-/usr/lib/dracut/skipcpio /boot/initramfs-$release.img | zcat | cpio -ivd
-cd -
+extract_initrd_into_initoverlayfs() {
+  cd /boot/initoverlayfs/
+  /usr/lib/dracut/skipcpio /boot/initramfs-$release.img | zcat | cpio -ivd
+  cd -
+}
 
+extract_initrd_into_initoverlayfs
 gcc -O3 -pedantic -Wall -Wextra initoverlayfs2init.c -o /boot/initoverlayfs/usr/bin/initoverlayfs2init
 sed -i '/^initrd /d' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
 sed -i 's#options root=UUID=2f8957f6-9ecd-480f-b738-41d6da946bf4 ro rhgb quiet#root=/dev/vda3 ro rhgb quiet rootfstype=ext4 rootwait init=/usr/bin/initoverlayfs2init#g' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
 sed -i "s/UUID=2aadcf0d-81dc-4b21-99ef-74b96bb357ad/# UUID=2aadcf0d-81dc-4b21-99ef-74b96bb357ad/g" /etc/fstab
 systemctl daemon-reload
 dracut -f
+extract_initrd_into_initoverlayfs # a second time to get the new fstab into initoverlayfs
 
