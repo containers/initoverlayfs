54fa87c5afeb24729df041675f54215dc0a2ae7d
Eric Curtin <ecurtin@redhat.com>
2023-07-12 13:04:57 +0100

    add path to install

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-gcc -O3 -pedantic -Wall -Wextra initoverlayfs2init.c -o initoverlayfs2init
+gcc -O3 -pedantic -Wall -Wextra initoverlayfs2init.c -o /boot/initoverlayfs/usr/bin/initoverlayfs2init
 
