cad567672c78ec77c74d067a16588c11c493cb88
Eric Curtin <ecurtin@redhat.com>
2023-07-24 20:12:07 +0100

    changes

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ epoch=$(date +%s)
 journalctl --output=short-monotonic > journalctl$epoch.txt
 journalctl --output=short-monotonic | grep -i "Reached target" > reached_target$epoch.txt
 sed -i "s/UUID=2aadcf0d-81dc-4b21-99ef-74b96bb357ad/# UUID=2aadcf0d-81dc-4b21-99ef-74b96bb357ad/g" /etc/fstab
+if false; then
 cp mount-sysroot.service /usr/lib/systemd/system/
 mkdir -p /usr/lib/dracut/modules.d/00early-boot-service
 echo '#!/usr/bin/bash
@@ -32,8 +33,9 @@ chcon system_u:object_r:systemd_unit_file_t:s0 /usr/lib/systemd/system/mount-sys
 cd /usr/lib/systemd/system/sysinit.target.wants/
 ln -s ../mount-sysroot.service
 cd -
+fi
 systemctl daemon-reload
-dracut -f --compress=pigz
+# dracut -f --compress=pigz
 
 extract_initrd_into_initoverlayfs
 
@@ -43,5 +45,5 @@ gcc -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O3 -pedantic -Wall -Wextra inito
 sed -i '/^initrd /d' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
 
 # should be ro rhgb quiet, cannot remount ro, but can fix
-sed -i 's#options root=UUID=2f8957f6-9ecd-480f-b738-41d6da946bf4 ro#options root=/dev/vda3 rw rootfstype=ext4 rootwait init=/usr/sbin/initoverlayfs2init#g' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
+sed -i 's#options root=UUID=2f8957f6-9ecd-480f-b738-41d6da946bf4 ro#options root=/dev/vda3 rw rootfstype=ext4 rootwait init=/usr/sbin/initoverlayfs2init root=/dev/vda4#g' /boot/loader/entries/9c03d22e1ec14ddaac4f0dabb884e434-$release.conf
 
