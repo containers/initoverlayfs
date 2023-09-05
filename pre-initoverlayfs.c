2e6e12f12fa50e2ef78be234fb24d2f2609bb2f8
Eric Curtin <ecurtin@redhat.com>
2023-09-05 12:24:15 +0100

    MS_RDONLY

--- pre-initoverlayfs.c
+++ pre-initoverlayfs.c
@@ -415,8 +415,8 @@ printd("Finish udevadm\n");
 
     fork_exec_absolute("/usr/sbin/modprobe", "loop");
     fork_exec_absolute("/usr/sbin/losetup", "/dev/loop0", file);
-    if (mount("/dev/loop0", "/initerofs", "erofs", 0, NULL))
-      print("mount(\"/dev/loop0\", \"/initerofs\", \"erofs\", 0, NULL) %d (%s)\n", errno, strerror(errno));
+    if (mount("/dev/loop0", "/initerofs", "erofs", MS_RDONLY, NULL))
+      print("mount(\"/dev/loop0\", \"/initerofs\", \"erofs\", MS_RDONLY, NULL) %d (%s)\n", errno, strerror(errno));
 
     exec_path("bash");
     printd("Finish mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed with errno: %d\n", part, errno);
