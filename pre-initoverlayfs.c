955f93374c9083e05aae93ccaed9e45e95f97e27
Eric Curtin <ecurtin@redhat.com>
2023-09-05 21:59:48 +0100

    format

--- pre-initoverlayfs.c
+++ pre-initoverlayfs.c
@@ -192,115 +192,108 @@ static inline char* read_proc_cmdline_key(const char* key) {
 }
 #endif
 
-static inline char *
-find_proc_cmdline_key (const char *cmdline, const char *key)
-{
-  const size_t key_len = strlen (key);
-  for (const char *iter = cmdline; iter;)
-    {
-      const char *next = strchr (iter, ' ');
-      if (strncmp (iter, key, key_len) == 0 && iter[key_len] == '=')
-        {
-          const char *start = iter + key_len + 1;
-          if (next)
-            return strndup (start, next - start);
-
-          return strdup (start);
-        }
-
+static inline char* find_proc_cmdline_key(const char* cmdline,
+                                          const char* key) {
+  const size_t key_len = strlen(key);
+  for (const char* iter = cmdline; iter;) {
+    const char* next = strchr(iter, ' ');
+    if (strncmp(iter, key, key_len) == 0 && iter[key_len] == '=') {
+      const char* start = iter + key_len + 1;
       if (next)
-        next += strspn (next, " ");
+        return strndup(start, next - start);
 
-      iter = next;
+      return strdup(start);
     }
+
+    if (next)
+      next += strspn(next, " ");
+
+    iter = next;
+  }
 
   return NULL;
 }
 
-static inline bool
-string_contains(const char *cmdline, const char c) {
+static inline bool string_contains(const char* cmdline, const char c) {
   for (; cmdline; ++cmdline)
-      if (*cmdline == c)
-        return true;
+    if (*cmdline == c)
+      return true;
 
   return false;
 }
 
 static FILE* kmsg_f = 0;
 
-#define print(...) \
-  do {                   \
-    if (kmsg_f) { \
+#define print(...)                  \
+  do {                              \
+    if (kmsg_f) {                   \
       fprintf(kmsg_f, __VA_ARGS__); \
-      break; \
-    } \
-      \
-    printf(__VA_ARGS__); \
+      break;                        \
+    }                               \
+                                    \
+    printf(__VA_ARGS__);            \
   } while (0)
 
-#define printd(...)      \
-  do {                   \
+#define printd(...)     \
+  do {                  \
     print(__VA_ARGS__); \
   } while (0)
 
-#define fork_exec_absolute(exe, ...) \
-do { \
+#define fork_exec_absolute(exe, ...)           \
+  do {                                         \
+    printd("execl(\"%s\")\n", exe);            \
+    const pid_t pid = fork();                  \
+    if (pid == -1) {                           \
+      print("fail exec_absolute\n");           \
+      break;                                   \
+    } else if (pid > 0) {                      \
+      waitpid(pid, 0, 0);                      \
+      break;                                   \
+    }                                          \
+                                               \
+    execl(exe, exe, __VA_ARGS__, (char*)NULL); \
+  } while (0)
+
+#define exec_absolute_no_wait(exe)           \
+  do {                                       \
+    printd("execl(\"%s\")\n", exe);          \
+    const pid_t pid = fork();                \
+    if (pid == -1) {                         \
+      print("fail exec_absolute_no_wait\n"); \
+      break;                                 \
+    } else if (pid > 0) {                    \
+      break;                                 \
+    }                                        \
+                                             \
+    execl(exe, exe, (char*)NULL);            \
+  } while (0)
+
+#define fork_exec_path(exe, ...)                \
+  do {                                          \
+    printd("execlp(\"%s\")\n", exe);            \
+    const pid_t pid = fork();                   \
+    if (pid == -1) {                            \
+      print("fail exec_path\n");                \
+      break;                                    \
+    } else if (pid > 0) {                       \
+      waitpid(pid, 0, 0);                       \
+      break;                                    \
+    }                                           \
+                                                \
+    execlp(exe, exe, __VA_ARGS__, (char*)NULL); \
+  } while (0)
+
+#define exec_path(exe)               \
+  do {                               \
+    printd("execlp(\"%s\")\n", exe); \
+    execlp(exe, exe, (char*)NULL);   \
+  } while (0)
+
+#define exec_absolute_path(exe)     \
+  do {                              \
     printd("execl(\"%s\")\n", exe); \
-const pid_t pid = fork(); \
-if (pid == -1) { \
-  print("fail exec_absolute\n"); \
-  break; \
-} \
-else if (pid > 0) { \
-    waitpid(pid, 0, 0); \
-    break; \
-} \
-\
-    execl(exe, exe, __VA_ARGS__, (char*) NULL); \
-} while (0)
-
-#define exec_absolute_no_wait(exe) \
-do { \
-    printd("execl(\"%s\")\n", exe); \
-const pid_t pid = fork(); \
-if (pid == -1) { \
-  print("fail exec_absolute_no_wait\n"); \
-  break; \
-} \
-else if (pid > 0) { \
-    break; \
-} \
-\
-    execl(exe, exe, (char*) NULL); \
-} while (0)
-
-#define fork_exec_path(exe, ...) \
-do { \
-  printd("execlp(\"%s\")\n", exe); \
-const pid_t pid = fork(); \
-if (pid == -1) { \
-  print("fail exec_path\n"); \
-  break; \
-} \
-else if (pid > 0) { \
-    waitpid(pid, 0, 0); \
-    break; \
-} \
-\
-  execlp(exe, exe, __VA_ARGS__, (char*) NULL); \
-} while (0)
-
-#define exec_path(exe) \
-do { \
-  printd("execlp(\"%s\")\n", exe); \
-  execlp(exe, exe, (char*) NULL); \
-} while (0)
-
-#define exec_absolute_path(exe) \
-do { \
-  printd("execl(\"%s\")\n", exe); \
-  execl(exe, exe, (char*) NULL); \
-} while (0)
+    execl(exe, exe, (char*)NULL);   \
+  } while (0)
 
 #if 0
 static inline int print_dev(void)
@@ -360,34 +353,44 @@ static inline int fd_move_above_stdio(int fd) {
 #endif
 
 static inline int log_open_kmsg(void) {
-        kmsg_f = fopen("/dev/kmsg", "w");
-        if (!kmsg_f) {
-                print("open(\"/dev/kmsg\", \"w\"), %d = errno", errno);
-                return errno;
-        }
+  kmsg_f = fopen("/dev/kmsg", "w");
+  if (!kmsg_f) {
+    print("open(\"/dev/kmsg\", \"w\"), %d = errno", errno);
+    return errno;
+  }
 
-        setvbuf(kmsg_f, 0, _IOLBF, 0);
-        return 0;
+  setvbuf(kmsg_f, 0, _IOLBF, 0);
+  return 0;
 }
 
-static inline int pivot_root(const char *new_root, const char *put_old) {
+static inline int pivot_root(const char* new_root, const char* put_old) {
   return syscall(SYS_pivot_root, new_root, put_old);
 }
 
 int main(void) {
   print("Start pre-initoverlayfs\n");
-  if (mount("proc", "/proc", "proc", MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL)) {
-    print("mount(\"proc\", \"/proc\", \"proc\", MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL) failed with errno: %d\n", errno);
+  if (mount("proc", "/proc", "proc", MS_NOSUID | MS_NOEXEC | MS_NODEV, NULL)) {
+    print(
+        "mount(\"proc\", \"/proc\", \"proc\", MS_NOSUID|MS_NOEXEC|MS_NODEV, "
+        "NULL) failed with errno: %d\n",
+        errno);
     return errno;
   }
 
-  if (mount("sysfs", "/sys", "sysfs", MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL)) {
-    print("mount(\"sysfs\", \"/sys\", \"sysfs\", MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL) failed with errno: %d\n", errno);
+  if (mount("sysfs", "/sys", "sysfs", MS_NOSUID | MS_NOEXEC | MS_NODEV, NULL)) {
+    print(
+        "mount(\"sysfs\", \"/sys\", \"sysfs\", MS_NOSUID|MS_NOEXEC|MS_NODEV, "
+        "NULL) failed with errno: %d\n",
+        errno);
     return errno;
   }
 
-  if (mount("devtmpfs", "/dev", "devtmpfs", MS_NOSUID|MS_STRICTATIME, "mode=0755,size=4m")) {
-    print("mount(\"devtmpfs\", \"/dev\", \"devtmpfs\", MS_NOSUID|MS_STRICTATIME, NULL) failed with errno: %d\n", errno);
+  if (mount("devtmpfs", "/dev", "devtmpfs", MS_NOSUID | MS_STRICTATIME,
+            "mode=0755,size=4m")) {
+    print(
+        "mount(\"devtmpfs\", \"/dev\", \"devtmpfs\", MS_NOSUID|MS_STRICTATIME, "
+        "NULL) failed with errno: %d\n",
+        errno);
     return errno;
   }
 
@@ -395,51 +398,78 @@ int main(void) {
   print_dev();
 #endif
 
-log_open_kmsg();
-printd("Start systemd-udevd\n");
-fork_exec_absolute("/lib/systemd/systemd-udevd", "--daemon");
-printd("Start udevadm\n");
-fork_exec_path("udevadm", "trigger", "--type=devices", "--action=add" , "--subsystem-match=module", "--subsystem-match=block", "--subsystem-match=virtio", "--subsystem-match=pci", "--subsystem-match=nvme" , "-w");
-printd("Finish udevadm\n");
+  log_open_kmsg();
+  printd("Start systemd-udevd\n");
+  fork_exec_absolute("/lib/systemd/systemd-udevd", "--daemon");
+  printd("Start udevadm\n");
+  fork_exec_path("udevadm", "trigger", "--type=devices", "--action=add",
+                 "--subsystem-match=module", "--subsystem-match=block",
+                 "--subsystem-match=virtio", "--subsystem-match=pci",
+                 "--subsystem-match=nvme", "-w");
+  printd("Finish udevadm\n");
 
   printd("Start read_proc_cmdline\n");
-  autofree char *cmdline = read_proc_cmdline ();
+  autofree char* cmdline = read_proc_cmdline();
   printd("Finish read_proc_cmdline\n");
   if (!cmdline) {
     printd("cmdline: NULL\n");
   }
 
   printd("cmdline: \"%s\"\n", cmdline);
-  autofree char* initoverlayfs = find_proc_cmdline_key(cmdline, "initoverlayfs");
+  autofree char* initoverlayfs =
+      find_proc_cmdline_key(cmdline, "initoverlayfs");
 
-  printd("cmdline: \"%s\" initoverlayfs: \"%s\"\n", cmdline, initoverlayfs ? initoverlayfs : "NULL");
+  printd("cmdline: \"%s\" initoverlayfs: \"%s\"\n", cmdline,
+         initoverlayfs ? initoverlayfs : "NULL");
 
   if (string_contains(initoverlayfs, ':')) {
     strtok(initoverlayfs, ":");
     const char* file = strtok(NULL, ":");
     const char* part = initoverlayfs;
-    printd("Start mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed with errno: %d\n", part, errno);
+    printd(
+        "Start mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed with "
+        "errno: %d\n",
+        part, errno);
 
     if (mount(part, "/boot", "ext4", MS_RDONLY, NULL))
-      print("mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed with errno: %d\n", part, errno);
+      print(
+          "mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed with "
+          "errno: %d\n",
+          part, errno);
 
     fork_exec_absolute("/usr/sbin/modprobe", "loop");
     fork_exec_absolute("/usr/sbin/losetup", "/dev/loop0", file);
     if (mount("/dev/loop0", "/initerofs", "erofs", MS_RDONLY, NULL))
-      print("mount(\"/dev/loop0\", \"/initerofs\", \"erofs\", MS_RDONLY, NULL) %d (%s)\n", errno, strerror(errno));
+      print(
+          "mount(\"/dev/loop0\", \"/initerofs\", \"erofs\", MS_RDONLY, NULL) "
+          "%d (%s)\n",
+          errno, strerror(errno));
 
-    if (mount("overlay", "/initoverlayfs", "overlay", 0, "redirect_dir=on,lowerdir=/initerofs,upperdir=/overlay/upper,workdir=/overlay/work"))
-      print("mount(\"overlay\", \"/initoverlayfs\", \"overlay\", 0, \"redirect_dir=on,lowerdir=/initerofs,upperdir=/overlay/upper,workdir=/overlay/work\") %d (%s)\n", errno, strerror(errno));
+    if (mount("overlay", "/initoverlayfs", "overlay", 0,
+              "redirect_dir=on,lowerdir=/initerofs,upperdir=/overlay/"
+              "upper,workdir=/overlay/work"))
+      print(
+          "mount(\"overlay\", \"/initoverlayfs\", \"overlay\", 0, "
+          "\"redirect_dir=on,lowerdir=/initerofs,upperdir=/overlay/"
+          "upper,workdir=/overlay/work\") %d (%s)\n",
+          errno, strerror(errno));
 
     if (mount("/boot", "/initoverlayfs/boot", "ext4", MS_MOVE, NULL))
-      print("mount(\"/boot\", \"/initoverlayfs/boot\", \"ext4\", MS_MOVE, NULL) %d (%s)\n", errno, strerror(errno));
+      print(
+          "mount(\"/boot\", \"/initoverlayfs/boot\", \"ext4\", MS_MOVE, NULL) "
+          "%d (%s)\n",
+          errno, strerror(errno));
 
     if (pivot_root("/initoverlayfs", "/"))
-      print("pivot_root(\"initoverlayfs\", \"/\") %d (%s)\n", errno, strerror(errno));
+      print("pivot_root(\"initoverlayfs\", \"/\") %d (%s)\n", errno,
+            strerror(errno));
 
     exec_absolute_path("/sbin/init");
-//    exec_path("bash");
-    printd("Finish mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed with errno: %d\n", part, errno);
+    //    exec_path("bash");
+    printd(
+        "Finish mount(\"%s\", \"/boot\", \"ext4\", MS_RDONLY, NULL) failed "
+        "with errno: %d\n",
+        part, errno);
   }
 
   fclose(kmsg_f);
