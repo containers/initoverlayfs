b8174300afb0cfdb1ccb355a7b6fc91a6e438d50
Eric Curtin <ecurtin@redhat.com>
2023-09-06 16:34:09 +0100

    try again

--- lib/dracut/modules.d/81pre-initramfs/module-setup.sh
+++ lib/dracut/modules.d/81pre-initramfs/module-setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 installkernel() {
-    hostonly='' instmods erofs
+    hostonly='' instmods erofs overlay
 }
 
 install() {
     inst /usr/sbin/pre-initoverlayfs
-#    inst_dir /boot /initerofs /overlay /overlay/upper /overlay/work /initoverlayfs
+    inst_dir /boot /initerofs /overlay /overlay/upper /overlay/work /initoverlayfs
 }
 
