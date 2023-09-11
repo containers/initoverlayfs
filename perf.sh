431af7d5c7d85e6a19d9ec78b2de3b29be50e8c7
Eric Curtin <ecurtin@redhat.com>
2023-09-11 21:17:06 +0100

    legacy initoverlayfs

--- perf.sh
+++ perf.sh
@@ -7,10 +7,10 @@ for i in {1..100} ; do
   cp f38.qcow2.bak f38.qcow2; ./runvm --nographics f38.qcow2 &
   cd -
   sleep 32
-  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-monotonic -b | grep \"Reached target\"" > legacy$i.txt
+  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-monotonic -b" > legacy$i.txt
   git-push.sh -p2222 ecurtin@127.0.0.1 && ssh -p2222 ecurtin@127.0.0.1 "cd ~/git/initoverlayfs && ./build.sh && sudo reboot"
   sleep 32
-  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-monotonic -b | grep \"Reached target\"" > initoverlayfs$i.txt
+  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-monotonic -b" > initoverlayfs$i.txt
   pkill qemu
 done
 
