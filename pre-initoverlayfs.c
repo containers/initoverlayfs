16a8671898a1fc431c7ec5527b9c1910f4ed9eab
Eric Curtin <ecurtin@redhat.com>
2023-09-11 11:53:57 +0100

    switch

--- pre-initoverlayfs.c
+++ pre-initoverlayfs.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/loop.h>
+#include <linux/magic.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -169,10 +170,6 @@ static inline int log_open_kmsg(void) {
   return 0;
 }
 
-static inline int pivot_root(const char* new_root, const char* put_old) {
-  return syscall(SYS_pivot_root, new_root, put_old);
-}
-
 static inline int losetup(char* loopdev, const char* file) {
   autoclose const int loopctlfd = open("/dev/loop-control", O_RDWR | O_CLOEXEC);
   if (loopctlfd < 0) {
@@ -227,6 +224,173 @@ static inline int losetup(char* loopdev, const char* file) {
   }
 
   return 0;
+}
+
+/* remove all files/directories below dirName -- don't cross mountpoints */
+static inline int recursiveRemove(int fd) {
+  struct stat rb;
+  DIR* dir;
+  int rc = -1;
+  int dfd;
+
+  if (!(dir = fdopendir(fd))) {
+    print("failed to open directory");
+    goto done;
+  }
+
+  /* fdopendir() precludes us from continuing to use the input fd */
+  dfd = dirfd(dir);
+  if (fstat(dfd, &rb)) {
+    print("stat failed");
+    goto done;
+  }
+
+  while (1) {
+    struct dirent* d;
+    int isdir = 0;
+
+    errno = 0;
+    if (!(d = readdir(dir))) {
+      if (errno) {
+        print("failed to read directory");
+        goto done;
+      }
+      break; /* end of directory */
+    }
+
+    if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, ".."))
+      continue;
+#ifdef _DIRENT_HAVE_D_TYPE
+    if (d->d_type == DT_DIR || d->d_type == DT_UNKNOWN)
+#endif
+    {
+      struct stat sb;
+
+      if (fstatat(dfd, d->d_name, &sb, AT_SYMLINK_NOFOLLOW)) {
+        print("stat of %s failed", d->d_name);
+        continue;
+      }
+
+      /* skip if device is not the same */
+      if (sb.st_dev != rb.st_dev)
+        continue;
+
+      /* remove subdirectories */
+      if (S_ISDIR(sb.st_mode)) {
+        int cfd;
+
+        cfd = openat(dfd, d->d_name, O_RDONLY);
+        if (cfd >= 0)
+          recursiveRemove(cfd); /* it closes cfd too */
+        isdir = 1;
+      }
+    }
+
+    if (unlinkat(dfd, d->d_name, isdir ? AT_REMOVEDIR : 0))
+      print("failed to unlink %s", d->d_name);
+  }
+
+  rc = 0; /* success */
+done:
+  if (dir)
+    closedir(dir);
+  else
+    close(fd);
+  return rc;
+}
+
+static inline int switchroot(const char* newroot) {
+  /*  Don't try to unmount the old "/", there's no way to do it. */
+  const char* umounts[] = {"/dev", "/proc", "/sys", "/run", NULL};
+  int i;
+  int cfd = -1;
+  struct stat newroot_stat, oldroot_stat, sb;
+
+  if (stat("/", &oldroot_stat) != 0) {
+    print("stat of %s failed", "/");
+    return -1;
+  }
+
+  if (stat(newroot, &newroot_stat) != 0) {
+    print("stat of %s failed", newroot);
+    return -1;
+  }
+
+  for (i = 0; umounts[i] != NULL; i++) {
+    char newmount[PATH_MAX];
+
+    snprintf(newmount, sizeof(newmount), "%s%s", newroot, umounts[i]);
+
+    if ((stat(umounts[i], &sb) == 0) && sb.st_dev == oldroot_stat.st_dev) {
+      /* mount point to move seems to be a normal directory or stat failed */
+      continue;
+    }
+
+    if ((stat(newmount, &sb) != 0) || (sb.st_dev != newroot_stat.st_dev)) {
+      /* mount point seems to be mounted already or stat failed */
+      umount2(umounts[i], MNT_DETACH);
+      continue;
+    }
+
+    if (mount(umounts[i], newmount, NULL, MS_MOVE, NULL) < 0) {
+      print("failed to mount moving %s to %s", umounts[i], newmount);
+      print("forcing unmount of %s", umounts[i]);
+      umount2(umounts[i], MNT_FORCE);
+    }
+  }
+
+  if (chdir(newroot)) {
+    print("failed to change directory to %s", newroot);
+    return -1;
+  }
+
+  cfd = open("/", O_RDONLY);
+  if (cfd < 0) {
+    print("cannot open %s", "/");
+    goto fail;
+  }
+
+  if (mount(newroot, "/", NULL, MS_MOVE, NULL) < 0) {
+    print("failed to mount moving %s to /", newroot);
+    goto fail;
+  }
+
+  if (chroot(".")) {
+    print("failed to change root");
+    goto fail;
+  }
+
+  if (chdir("/")) {
+    print("cannot change directory to %s", "/");
+    goto fail;
+  }
+
+  switch (fork()) {
+    case 0: /* child */
+    {
+      struct statfs stfs;
+
+      if (fstatfs(cfd, &stfs) == 0 &&
+          (stfs.f_type == RAMFS_MAGIC || stfs.f_type == TMPFS_MAGIC))
+        recursiveRemove(cfd);
+      else {
+        print("old root filesystem is not an initramfs");
+        close(cfd);
+      }
+      exit(EXIT_SUCCESS);
+    }
+    case -1: /* error */
+      break;
+
+    default: /* parent */
+      close(cfd);
+      return 0;
+  }
+
+fail:
+  if (cfd >= 0)
+    close(cfd);
+  return -1;
 }
 
 int main(void) {
@@ -320,9 +484,8 @@ int main(void) {
           "%d (%s)\n",
           errno, strerror(errno));
 
-    if (pivot_root("/initoverlayfs", "/"))
-      print("pivot_root(\"initoverlayfs\", \"/\") %d (%s)\n", errno,
-            strerror(errno));
+    if (switchroot("/initoverlayfs"))
+      print("switchroot(\"initoverlayfs\") %d (%s)\n", errno, strerror(errno));
 
     exec_path("bash");
     exec_absolute_path("/sbin/init");
