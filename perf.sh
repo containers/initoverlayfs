4db7087d13dee782954442746364e1c0e4646e11
Eric Curtin <ecurtin@redhat.com>
2023-09-12 17:19:14 +0100

    perf

--- perf.sh
+++ perf.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-for i in {1..100} ; do
+convert_file() {
+set +x
+
+  file="$1"
+
+  touch $file.bak
+
+  while read j; do
+    first_word=$(echo $j | awk '{print $1}')
+    rest_of_line=$(echo $j | sed 's/[^ ]* //')
+    difference=$(echo  "$first_word - $preboot_time" | bc)
+    echo "$difference $rest_of_line" >> $file.bak
+  done < $file
+
+  mv $file.bak $file
+}
+
+pkill qemu || true
+
+for i in {1..64}; do
   cd
-  cp f38.qcow2.bak f38.qcow2; ./runvm --nographics f38.qcow2 &
+  cp f38.qcow2.bak f38.qcow2
+  preboot_time=$(date +%s.%N)
+  wait
+  ./runvm --nographics f38.qcow2 > /dev/null 2>&1 &
   cd -
   sleep 32
-  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-monotonic -b" > legacy$i.txt
-  git-push.sh -p2222 ecurtin@127.0.0.1 && ssh -p2222 ecurtin@127.0.0.1 "cd ~/git/initoverlayfs && ./build.sh && sudo reboot"
+  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-unix -b" > legacy$i.txt
+  convert_file legacy$i.txt &
+  git-push.sh -p2222 ecurtin@127.0.0.1
+  ssh -p2222 ecurtin@127.0.0.1 "cd ~/git/initoverlayfs && ./build.sh && sudo init 0" > /dev/null 2>&1
+
+  cd
+  preboot_time=$(date +%s.%N)
+  wait
+  ./runvm --nographics f38.qcow2 > /dev/null 2>&1 &
+  cd -
   sleep 32
-  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-monotonic -b" > initoverlayfs$i.txt
-  pkill qemu
+  ssh -p2222 ecurtin@127.0.0.1 "sudo journalctl --output=short-unix -b" > initoverlayfs$i.txt
+  convert_file initoverlayfs$i.txt &
+  ssh -p2222 ecurtin@127.0.0.1 "sudo init 0"
 done
 
