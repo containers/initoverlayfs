1966949c30167ad41ee996f597b9a7db83491323
Eric Curtin <ecurtin@redhat.com>
2023-09-18 16:57:40 +0000

    changes

--- build.sh
+++ build.sh
@@ -107,7 +107,9 @@ sudo cp -r lib/dracut/modules.d/81pre-initramfs /usr/lib/dracut/modules.d/
 
 set -x
 
-sudo dracut --lz4 -v -m "systemd kernel-modules udev-rules pre-initramfs" -f --strip -M -o "nss-softokn" # bash i18n kernel-modules-extra rootfs-block dracut-systemd usrmount base fs-lib shutdown # systemd-initrd (req by systemd)
+sudo clang -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O3 -pedantic -Wall -Wextra -Werror pre-initoverlayfs.c -o /usr/sbin/pre-initoverlayfs
+sudo gcc -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O3 -pedantic -Wall -Wextra -Werror -fanalyzer pre-initoverlayfs.c -o /usr/sbin/pre-initoverlayfs
+sudo dracut --lz4 -v -m "systemd kernel-modules udev-rules pre-initramfs" -f --strip -M -o "nss-softokn bash i18n kernel-modules-extra rootfs-block dracut-systemd usrmount base fs-lib shutdown" # systemd-initrd (req by systemd)
 sudo du -sh /boot/initramfs*
 #sudo lsinitrd | grep "init\|boot\|overlay\|erofs"
 sudo du -sh $initramfs
