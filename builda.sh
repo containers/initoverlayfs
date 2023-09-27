f67a28cc310f019dc796c0cb7ac58b1a073c1618
Eric Curtin <ecurtin@redhat.com>
2023-09-27 13:21:56 +0100

    rm files

--- builda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-UNLOCK_OVERLAYDIR="/var/tmp/initoverlay"
-mkdir -p "$UNLOCK_OVERLAYDIR/upper" "$UNLOCK_OVERLAYDIR/work"
-gcc -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O3 -pedantic -Wall -Wextra initoverlayfs2init.c -o initoverlayfs2init
-
