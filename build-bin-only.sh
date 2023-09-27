23c47482d86a7fe48ea44c2fa04f28795269ef77
Eric Curtin <ecurtin@redhat.com>
2023-09-27 15:59:21 +0100

    remove UNLOCK_OVERLAYDIR define

--- build-bin-only.sh
+++ build-bin-only.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-clang++ -o a -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O3 -pedantic -Wall -Wextra -Werror -Wno-language-extension-token -Wno-deprecated -std=c++20 pre-init.c &
-g++ -o b -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O0 -ggdb -pedantic -Wall -Wextra -Werror -Wno-language-extension-token -std=c++20 pre-init.c &
-clang -o c -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O3 -pedantic -Wall -Wextra -Werror -Wno-language-extension-token pre-init.c &
+clang++ -o a -O3 -pedantic -Wall -Wextra -Werror -Wno-language-extension-token -Wno-deprecated -std=c++20 pre-init.c &
+g++ -o b -O0 -ggdb -pedantic -Wall -Wextra -Werror -Wno-language-extension-token -std=c++20 pre-init.c &
+clang -o c -O3 -pedantic -Wall -Wextra -Werror -Wno-language-extension-token pre-init.c &
 gcc -O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -fstack-protector-strong -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection pre-init.c -o d &
 
 wait
 
-gcc -DUNLOCK_OVERLAYDIR=\"$UNLOCK_OVERLAYDIR\" -O0 -ggdb -pedantic -Wall -Wextra -Werror -Wno-language-extension-token -fanalyzer pre-init.c
+gcc -O0 -ggdb -pedantic -Wall -Wextra -Werror -Wno-language-extension-token -fanalyzer pre-init.c
 valgrind ./a.out
 
