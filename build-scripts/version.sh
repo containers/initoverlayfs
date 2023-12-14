4c998f997c3e4b75d5bb307fba8aab38d773aaca
Yariv Rachmani <yrachman@redhat.com>
2023-12-14 13:46:50 +0200

    Adding support with versioning for non snapshot builds
    Signed-off-by: Yariv Rachmani <yrachman@redhat.com>

---
+++ build-scripts/version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# Current version of initoverlayfs
+VERSION=0.98
+# Specify if build is a official release or a snapshot build
+IS_RELEASE=false
+# Used for official releases. Increment if necessary
+RELEASE="1"
+
+function short(){
+    echo ${VERSION}
+}
+
+function long(){
+    echo "$(short)-$(release)"
+}
+
+function release(){
+    # Package release
+
+    if [ $IS_RELEASE = false ]; then
+        # Used for nightly builds
+        RELEASE="0.$(date +%04Y%02m%02d%02H%02M).git$(git rev-parse --short ${GITHUB_SHA:-HEAD})"
+    fi
+    echo $RELEASE
+}
+
+[ -z $1 ] && short || $1
