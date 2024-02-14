21a5662285f08f08cf8e7939c496765e58ca26c7
Eric Curtin <ecurtin@redhat.com>
2024-02-14 12:31:51 +0000

    Reintroduce initoverlayfs-init

--- lib/dracut/modules.d/81initoverlayfs/module-setup.sh
+++ lib/dracut/modules.d/81initoverlayfs/module-setup.sh
@@ -10,19 +10,29 @@ depends() {
 }
 
 install() {
-    inst_multiple -o /etc/initoverlayfs.conf /usr/sbin/initoverlayfs \
-      "$systemdsystemunitdir/pre-initoverlayfs.target" \
+  INITOVERLAYFS_CONF="/etc/initoverlayfs.conf"
+  INITOVERLAYFS_INIT=$(sed -ne "s/^initoverlayfs_init\s//pg" "$INITOVERLAYFS_CONF")
+  USE_SYSTEMD="true"
+  if [ "$INITOVERLAYFS_INIT" = "true" ]; then
+    USE_SYSTEMD="false"
+  fi
+
+  inst_multiple -o $INITOVERLAYFS_CONF /usr/sbin/initoverlayfs
+
+  if $USE_SYSTEMD; then
+    inst_multiple -o "$systemdsystemunitdir/pre-initoverlayfs.target" \
       "$systemdsystemunitdir/pre-initoverlayfs.service" \
       "$systemdsystemunitdir/pre-initoverlayfs-switch-root.service"
+  fi
 
+  inst_dir /boot /initrofs /overlay /overlay/upper /overlay/work /initoverlayfs
 
-    inst_dir /boot /initrofs /overlay /overlay/upper /overlay/work \
-      /initoverlayfs
-
+  if $USE_SYSTEMD; then
     $SYSTEMCTL -q --root "$initdir" set-default pre-initoverlayfs.target
     $SYSTEMCTL -q --root "$initdir" add-wants sysinit.target pre-initoverlayfs.service
     $SYSTEMCTL -q --root "$initdir" add-wants sysinit.target pre-initoverlayfs-switch-root.service
+  fi
 
-    > "${initdir}/usr/bin/bash"
+  > "${initdir}/usr/bin/bash"
 }
 
