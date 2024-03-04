77238db1e62bca9356ab48e16687c8edbd94370e
Eric Curtin <ecurtin@redhat.com>
2024-03-04 09:57:42 +0000

    bash

--- initoverlayfs.c
+++ initoverlayfs.c
@@ -558,7 +558,6 @@ static int mount_proc_sys_dev(void) {
     return errno;
   }
 
-  execl_single_arg("/usr/bin/bash");
   return 0;
 }
 
@@ -594,6 +593,7 @@ int main(int argc, char* argv[]) {
   autofclose FILE* kmsg_f_scoped = NULL;
   if (!systemd) {
     mount_proc_sys_dev();
+    execl_single_arg("/usr/bin/bash");
     log_open_kmsg();
     kmsg_f = kmsg_f_scoped;
   }
