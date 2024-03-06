224e95475110c96a3cc41439ee521d62a45480c3
Eric Curtin <ecurtin@redhat.com>
2024-03-06 13:14:26 +0000

    chnage

--- lib/dracut/modules.d/81early-service/module-setup.sh
+++ lib/dracut/modules.d/81early-service/module-setup.sh
@@ -2,7 +2,7 @@
 
 # called by dracut
 install() {
-    inst /usr/bin/binary-reader /usr/bin/binary
+    inst /usr/bin/binary-reader /usr/bin/binary /usr/lib/systemd/system/binary-reader.service
     $SYSTEMCTL -q --root "$initdir" enable binary-reader.service
 }
 
